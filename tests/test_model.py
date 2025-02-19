d36597b6eb0d4881c8e0fcb51980c3bf634782d2
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-02-19 18:44:20 +0530

    Flask verification in CI.yaml file

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,8 +7,6 @@ import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pickle
 
-
-
 class TestModelLoading(unittest.TestCase):
 
     @classmethod
