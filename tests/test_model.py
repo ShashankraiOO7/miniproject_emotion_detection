8d9e37591353b76f6066ddce6055763b9bc8a6e3
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-02-19 17:27:55 +0530

    After Adding Signature and Performance testing

--- tests/test_model.py
+++ tests/test_model.py
@@ -48,7 +48,7 @@ class TestModelLoading(unittest.TestCase):
     def test_model_loaded_properly(self):
         self.assertIsNotNone(self.new_model)
 
-    '''def test_model_signature(self):
+    def test_model_signature(self):
         # Create a dummy input for the model based on expected input shape
         input_text = "hi how are you"
         input_data = self.vectorizer.transform([input_text])
@@ -63,7 +63,7 @@ class TestModelLoading(unittest.TestCase):
         # Verify the output shape (assuming binary classification with a single output)
         self.assertEqual(len(prediction), input_df.shape[0])
         self.assertEqual(len(prediction.shape), 1)  # Assuming a single output column for binary classification
-
+# Setting the Min Certeria for model Prediction
     def test_model_performance(self):
         # Extract features and labels from holdout test data
         X_holdout = self.holdout_data.iloc[:,0:-1]
@@ -88,7 +88,7 @@ class TestModelLoading(unittest.TestCase):
         self.assertGreaterEqual(accuracy_new, expected_accuracy, f'Accuracy should be at least {expected_accuracy}')
         self.assertGreaterEqual(precision_new, expected_precision, f'Precision should be at least {expected_precision}')
         self.assertGreaterEqual(recall_new, expected_recall, f'Recall should be at least {expected_recall}')
-        self.assertGreaterEqual(f1_new, expected_f1, f'F1 score should be at least {expected_f1}')'''
+        self.assertGreaterEqual(f1_new, expected_f1, f'F1 score should be at least {expected_f1}')
 
 if __name__ == "__main__":
     unittest.main()
