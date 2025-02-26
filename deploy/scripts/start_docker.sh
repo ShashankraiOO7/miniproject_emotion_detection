62993ec8598d32c0c2a8e1c393c8ccaa28ca1344
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-02-26 17:42:40 +0530

    Creating Codedeploy for that appspec.yaml | deploy/scripts file added

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -10,4 +10,4 @@ docker stop my-container || true
 docker rm my-container || true
 
 # Run the Docker container
-docker run -d -p 80:5000 -e DAGSHUB_PAT=af702d31152ac81e1e55a4ca3ec2a47270c60970 --name shashankraiCICD  222634383382.dkr.ecr.ap-south-1.amazonaws.com/emotiondetection:1.0
+docker run -d -p 80:5000 -e DAGSHUB_PAT=af702d31152ac81e1e55a4ca3ec2a47270c60970 --name shashankraiCICD  222634383382.dkr.ecr.ap-south-1.amazonaws.com/emotiondetection:1.2
