07de2fd63d9b6de92d009eef36db423193834e70
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-02-26 17:52:21 +0530

    Creating Codedeploy for that appspec.yaml | deploy/scripts file added

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -6,8 +6,8 @@ docker pull 222634383382.dkr.ecr.ap-south-1.amazonaws.com/emotiondetection:1.0
 
 
 # Check if the container is already running or exists
-docker stop my-container || true
-docker rm my-container || true
+docker stop shashankraiCICD || true
+docker rm shashankraiCICD || true
 
 # Run the Docker container
 docker run -d -p 80:5000 -e DAGSHUB_PAT=af702d31152ac81e1e55a4ca3ec2a47270c60970 --name shashankraiCICD  222634383382.dkr.ecr.ap-south-1.amazonaws.com/emotiondetection:1.2
