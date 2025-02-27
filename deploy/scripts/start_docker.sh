9e06dbc1e92a73dea0632ac94ec64b5b0391b3f9
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-02-27 09:39:51 +0530

    Creating Codedeploy for that appspec.yaml | deploy/scripts file added

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,13 +1,21 @@
 #AWS Login
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 222634383382.dkr.ecr.ap-south-1.amazonaws.com
 
-docker pull 222634383382.dkr.ecr.ap-south-1.amazonaws.com/emotiondetection:1.0
+docker pull 222634383382.dkr.ecr.ap-south-1.amazonaws.com/emotiondetection:1.3
 
 
 
-# Check if the container is already running or exists
-docker stop shashankraiCICD || true
-docker rm shashankraiCICD || true
+# Check if the container 'campusx-app' is running
+if [ "$(docker ps -q -f name=shashankraiCICD)" ]; then
+    # Stop the running container
+    docker stop shashankraiCICD
+fi
+
+# Check if the container 'campusx-app' exists (stopped or running)
+if [ "$(docker ps -aq -f name=shashankraiCICD)" ]; then
+    # Remove the container if it exists
+    docker rm shashankraiCICD
+fi
 
 # Run the Docker container
-docker run -d -p 80:5000 -e DAGSHUB_PAT=af702d31152ac81e1e55a4ca3ec2a47270c60970 --name shashankraiCICD  222634383382.dkr.ecr.ap-south-1.amazonaws.com/emotiondetection:1.2
+docker run -d -p 80:5000 -e DAGSHUB_PAT=af702d31152ac81e1e55a4ca3ec2a47270c60970 --name shashankraiCICD  222634383382.dkr.ecr.ap-south-1.amazonaws.com/emotiondetection:1.3
